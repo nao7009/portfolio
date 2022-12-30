9d388caf1acc8369462babbd45d4aa1ae8ebc3de
shimojo <nao0909n@yahoo.co.jp>
2022-12-30 23:22:18 +0900

    favicon改修

--- vue.config.js
+++ vue.config.js
@@ -3,17 +3,17 @@ module.exports = {
   outputDir: 'docs',
   assetsDir: './',
   publicPath: './',
+  pages: {
+    index: {
+      entry: "src/main.js",
+      title: "portfolio"   
+    }
+  },
   css: {
     loaderOptions: {
       scss: {
         additionalData: `@import "@/assets/scss/_variables.scss";`
       }
-    }
-  },
-  pages: {
-    index: {
-      entry: "src/main.js",
-      title: "portfolio"   
     }
   }
 };
